54dafb749b600dca8950c7b5d6857710a4e02d43
prabhu150 <abhijeetprabhu15@gmail.com>
2019-09-24 14:39:26 -0700

    Automated gulp watch task to print minified css, isolated variables and made provisions for mixins/media-queries

---
+++ assignment-5/README.md
@@ -0,0 +1,8 @@
+#Assignment 5: Gulp and SASS
+
+
+Instructions to run:
+
+1. `npm install` to install required dependencies
+2. `npm install gulp -g` to install gulp globally in order to run gulp tasks. Use `sudo` on linux/OSX or run the shell with admin rights on Windows.
+3. `gulp sass` to run and watch the automated task of transpiling .scss to css
--- assignment-5/gulpfile.js
+++ assignment-5/gulpfile.js
@@ -1,15 +1,22 @@
- var {src,dest,watch} = require('gulp');
- var sass = require('gulp-sass')
- sass.compiler = require('node-sass')
+var gulp = require('gulp');
+var sass = require('gulp-sass')
+sass.compiler = require('node-sass')
+var rename = require('gulp-rename')
+let scss_path = './src/**/*.scss'
+let css_path = './dist/css/'
 
 
- function css()
- {
-    return src('src/*.scss')
-    .pipe(sass())
-    .pipe(dest('dist/css/'))
- }
-
- exports.default = ()=> {
-     watch('/src/*.scss',css)
- }
+gulp.task('sass:transpile',function(){
+   return gulp.src(scss_path)
+     .pipe(sass({
+      errLogToConsole: true,
+      outputStyle: 'expanded' // minifies style.min.css. Use expanded/compressed
+     }))
+     .pipe(rename('styles.min.css'))
+     .pipe(gulp.dest(css_path))
+     
+ });
+  
+gulp.task('sass', function () {
+   gulp.watch(scss_path, gulp.series(['sass:transpile']));
+ });
