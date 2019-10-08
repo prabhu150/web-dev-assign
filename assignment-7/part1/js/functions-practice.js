bad553d5c7726a7503dedf56df33edb1bcadeb71
prabhu150 <abhijeetprabhu15@gmail.com>
2019-10-07 17:35:27 -0700

    Made provisions for raw loops, comments about doubts

--- assignment-7/part1/js/functions-practice.js
+++ assignment-7/part1/js/functions-practice.js
@@ -27,12 +27,12 @@ function squareNumber(someNumber) {
 }
 
 //STEP 3
-function percentOf(firstNumber, secondNumber)
-{
-    let output,
-    percent = firstNumber / secondNumber * 100;
+function percentOf(firstNumber, secondNumber) {
+  let output,
+    percent = (firstNumber / secondNumber) * 100;
 
-  if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber)) output = errorMsg;
+  if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber))
+    output = errorMsg;
   else output = `${firstNumber} is ${percent}% of ${secondNumber}.`;
 
   console.log(output);
@@ -40,32 +40,47 @@ function percentOf(firstNumber, secondNumber)
 }
 //STEP 4
 
-function findModulus(firstNumber,secondNumber)
-{
-    let output,
-    modulus = secondNumber % firstNumber ;
+function findModulus(firstNumber, secondNumber) {
+  let output,
+    modulus = secondNumber % firstNumber;
 
-  if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber)) output = errorMsg;
-  else output = `${modulus} is the modulus of ${firstNumber} and ${secondNumber}.`;
+  if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber))
+    output = errorMsg;
+  else
+    output = `${modulus} is the modulus of ${firstNumber} and ${secondNumber}.`;
 
   console.log(output);
-  return modulus;   
+  return modulus;
 }
 
 //STEP 5
-/* should I write a seperate function to take the variable number of parameters or should I do that within the function itself? */
-function sumOfInput(...input)
-{
+/* 
+1. should I write a seperate function to take the variable number of parameters or should I do that within the function itself? 
 
-    let numbers = input.filter(value => Number.isInteger(value))
+2. are 2.3, 4.5 numbers? By numbers, do we mean integers or any non alphabetic characters?
+
+*/
+function sumOfInput(...input) {
+  let numbers = input.filter(value => Number.isInteger(value));
+
+  if (numbers.length > 0)
+    result = numbers.reduce((previousVal, currentVal) => {
+      return previousVal + currentVal;
+    });
+  else result = 0;
+
+  /*
+    Using raw loops if required 
     
-    if(numbers.length > 0)
-        result = numbers.reduce((previousVal,currentVal) => {return previousVal + currentVal;});
-    else
-        result = 0;
-
-    console.log(input)
-    console.log(`${result} is the sum for all integer arguments.`)
-    return result;
-
+    var size = input.length - 1, result = 0
+    while(size > -1)
+    {
+      if(Number.isInteger(input[size]))
+        result += input[size]
+      size--
+    }
+    */
+  console.log(input);
+  console.log(`${result} is the sum for all integer arguments.`);
+  return result;
 }
