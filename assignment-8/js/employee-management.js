93ae228b4c09a6ccf47b51e31bd94ef167f73198
prabhu150 <abhijeetprabhu15@gmail.com>
2019-10-14 13:57:56 -0700

    Added deletion. Comments and Refactoring.

--- assignment-8/js/employee-management.js
+++ assignment-8/js/employee-management.js
@@ -1,36 +1,101 @@
-employeeList = [
+/* 
+Starting off with 5 employees. This array will persist data 
+throughout app lifecycle
+*/
+var employeeList = [
   ["Sally Smith", "Quality Assurance", 3423],
   ["Mark Martin", "VP Sales", 3346],
-  ["John Johnson", "Marketing", 3232]
+  ["John Johnson", "Marketing", 3232],
+  ["Michael Douglas", "Security", 4431],
+  ["Sam Stone", "Staff", 4211]
 ];
 
-function $(elementId) {
-  return document.getElementById(elementId);
-}
-
-function updateTable() {
-  var table = $("EmployeeList");
-  console.log(`Loading ${employeeList.length} employees`);
-
-  $("numEmployees").innerHTML = `Showing  ${employeeList.length} employees`;
-
-  table.innerHTML = `<tr>
+const header = `<tr>
 <th>Name</th>
 <th>Title</th>
 <th>Extension</th>
 <th></th>
 </tr>`;
 
-  for (let employee of employeeList)
+/* This array will store the list of 
+delete buttons alongside their event listeners.
+Each button will belong to the same class but have a
+unique id identical to the employee they point to.
+*/
+var deleteButtons = [];
+
+function $(elementId) {
+  return document.getElementById(elementId);
+}
+
+/* This function loads the UI with all the elements
+found in the array. This will be called at the very beginning and 
+whenever an employee is deleted.
+*/
+function populateTable() {
+  console.log(`Loading ${employeeList.length} employees`);
+  var table = $("employeeList");
+
+  updateCount();
+
+  table.innerHTML = header;
+
+  for (let employee of employeeList) {
     table.innerHTML += `<tr><td>${employee[0]}</td><td>${employee[1]}</td><td>${
       employee[2]
     }</td>
-<td><button id=\"emp${employee[2]}\">Delete</button></td></tr>`;
+<td><button class= "deleteEmployee" id=\"${
+      employee[2]
+    }\">Delete</button></td></tr>`;
+  }
+  updateButtonListeners();
 }
 
+/* 
+This function will append the latest employee to the end of the list and simply 
+update the last row of the table and update the buttons array.
+*/
+function addEmployee(employee) {
+  var table = $("employeeList");
+  employeeList[employeeList.length] = employee;
+  updateCount();
+  table.innerHTML += `<tr><td>${employee[0]}</td><td>${employee[1]}</td><td>${
+    employee[2]
+  }</td>
+  <td><button class= "deleteEmployee" id=\"${
+    employee[2]
+  }\">Delete</button></td></tr>`;
 
-function removeEmployee(employeeId) {}
+  updateButtonListeners();
+}
 
+// This function reflects realtime count of employees in system.
+function updateCount() {
+  $("numEmployees").innerHTML = `Showing  ${employeeList.length} employees`;
+}
+
+/* This function simply attaches event listeners to each button and makes them point to
+ their employee object within the array */
+function updateButtonListeners() {
+  deleteButtons = document.getElementsByClassName("deleteEmployee");
+
+  for (let button of deleteButtons) {
+    button.addEventListener("click", item => {
+      console.log("Removing employee with id", item.target.id);
+      this.removeEmployee(item.target.id);
+    });
+  }
+}
+
+// Removes an employee and repopulates the table
+function removeEmployee(employeeId) {
+  employeeList = employeeList.filter(value => {
+    return value[2] != employeeId;
+  });
+  populateTable();
+}
+
+// Hides the error messages
 function clearErrors() {
   $("name-error").setAttribute("class", "hidden");
   $("title-error").setAttribute("class", "hidden");
@@ -40,7 +105,8 @@ function clearErrors() {
 function showError(element) {
   element.classList.remove("hidden");
 }
-
+/* Does form validation and adds an employee to system if user input is valid.
+ */
 function validateFormAndAddEmployee() {
   var isValid = true;
   clearErrors();
@@ -67,19 +133,27 @@ function validateFormAndAddEmployee() {
   }
 
   if (isValid == true) {
-    employeeList[employeeList.length] = [
-      name,
-      title,
-      parseInt(extension)
-    ];
-    updateTable();
-    $("employee-form").reset()
+    var newEmployee = [name, title, parseInt(extension)];
+    addEmployee(newEmployee);
+    $("employee-form").reset();
     $("name").focus();
   }
 }
+// Utility function for quick testing of app.
+function debugMode() {
+  $("name").value = "sample";
+  $("title").value = "marketing";
+  $("extension").value = 12345;
+}
 
 window.addEventListener("load", () => {
-  updateTable();
+  populateTable();
   $("name").focus();
-  $("employee-form").addEventListener("submit", this.validateFormAndAddEmployee);
+
+  //   debugMode() Uncomment to make testing easier
+
+  $("employee-form").addEventListener(
+    "submit",
+    this.validateFormAndAddEmployee
+  );
 });
